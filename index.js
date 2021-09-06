353d238b2d444d3c8f37606de1a327d60d439a50
suzonbachet <suzon.bachet@gmail.com>
2021-09-06 19:27:32 +0200

    create Express code to route all of the endpoints

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const { isError } = require('lodash');
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
     uuid = require('uuid');
@@ -61,16 +62,86 @@ let topMovies = [
     }
   ];
 
-app.get('/movies', (req,res) => {
-    res.json(topMovies)
-});
-
-app.get('/', (req, res) => {
-    res.send('Welcome to my movie app!');
-});
-
 app.use(express.static('public'));
 
+// Gets the list of data about all movies
+app.get('/movies', (req,res) => {
+    res.json(movies)
+});
+
+// Gets the description of a single movie, by title
+app.get('/movies/:title', (req, res) => {
+    res.send(movies.find((movie) =>
+        {return movie.title === req.params.title }));
+});
+
+// Gets data about a genre, by title
+app.get('/movies/:title/genre', (req, res) => {
+    res.send(movies.find((movie) =>
+        {return movie.title === req.params.title }));
+});
+
+// Gets data about a director, by name
+app.get('/directors/:name', (req, res) => {
+    res.send(directors.find((director) =>
+        {return director.name === req.params.name }));
+});
+
+// Gets data about a director, by name
+app.get('/directors/:name', (req, res) => {
+    res.send(directors.find((director) =>
+        {return director.name === req.params.name }));
+});
+
+// Add data for a new user to register
+app.post('/myaccount', (req, res) => {
+    let newUser = req.body;
+
+    if (!newUser.userName) {
+        const message = 'Missing user name in request body.';
+        res.status(400).send(message);
+    } else {
+        users.push(newUser);
+        res.status(201).send(newUser);
+    }
+});
+
+// Update user info by user name
+app.put('/myaccount', (req, res) => {
+    let user = users.find(user) => {
+        return user.userName === req.params.userName });
+    
+    if (!newUserName.userName) {
+        const message = 'Missing new user name in request body.';
+        res.status(400).send(message);
+    } else {
+        const message = 'User name has been changed.'
+        user.put(newUserName);
+        res.status(201.sens(newUserName);
+    });
+
+// Add a movie to the user's list of favorites
+app.put('/movies', (req, res) => {
+
+}
+
+// Remove a movie from the user's list of favorites
+app.delete('/mytopmovies', (req, res) => {
+
+}
+
+// Remove existing user
+app.delete('/myaccount', (req, res) => {
+    let user = users.find((user) => {
+        return user.userName = req.params.userName });
+
+    if (user) {
+        users = users.filter((obj) => {
+            return obj.userName !== req.params.userName });
+            res.status(201).send(req.params.id + 'was deleted.');
+    }
+});   
+  
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
