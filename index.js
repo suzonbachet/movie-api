1d9025773f53b490c73f6d60b62af13ba14d9862
suzonbachet <suzon.bachet@gmail.com>
2021-09-24 16:17:22 +0200

    add get request for all users and for user by name

--- index.js
+++ index.js
@@ -253,6 +253,30 @@ app.post('/users', (req, res) => {
         });
 });
 
+// Get all users
+app.get('/users', (req, res) => {
+    Users.find()
+        .then((users) => {
+            res.status(201).json(users);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
 
 // Update user info by user name
 app.put('/users/:username', (req, res) => {
