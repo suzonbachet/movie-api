c8437ded0943b9d6f33d663663996f305a894877
suzonbachet <suzon.bachet@gmail.com>
2021-10-08 17:04:11 +0200

    create a new endpoint for registered users to log in

--- auth.js
+++ auth.js
@@ -1,1 +1,35 @@
-auth
+const jwtSecret = 'your_jwt_secret'; // Has to be the same key used in the JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+    passport = require('passport');
+
+require('./passport'); // Local passport file
+
+let generateJWTToken = (user) => {
+    return jwt.sign(user, jwtSecret, {
+        subject: user.Username, // Username encoded in the JWT
+        expiresIn: '7d', // Token will expire in 7 days
+        algorithm: 'HS256' // Algorithm used to 'sign' or encode values of the JWT
+    });
+}
+
+/* POST login. */
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', { session: false}, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Something is not right.',
+                    user: user
+                });
+            }
+            req.login(user, { session: false}, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token });
+            });
+        })(req, res);
+    });
+}
