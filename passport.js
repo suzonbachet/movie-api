0d3698c8a160d2eb74f56c25ac9b09c8163b7a73
suzonbachet <suzon.bachet@gmail.com>
2021-10-13 12:31:52 +0200

    fix typo

--- passport.js
+++ passport.js
@@ -31,7 +31,7 @@ passport.use(new LocalStrategy({
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey: 'your_jwt_secret'
-}, (jwtPayload. callback) => {
+}, (jwtPayload.callback) => {
     return Users.findById(jwtPayload._id)
     .then((user) => {
         return callback(null, user);
